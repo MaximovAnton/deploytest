f90d50957ba8af584f840d5a60d548168645456c
Anton <scientistsmartbee@gmail.com>
2025-05-05 14:57:11 +0300

    deploytest

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ['rickandmortyapi.com'], // Добавьте сюда домен
+  },
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Component from './../../components/reactMemo'
+
+
+export default function Home() {
+  let username = {"name": "Leanne Graham"}
+
+  return (
+    <div> 
+      <Component />
+
+    </div>
+  );
+}
